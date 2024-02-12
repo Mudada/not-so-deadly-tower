a58d2c180406f923165c6a9dfaee8709198c3bbc
Mudada <mael.nicolas77@gmail.com>
2024-02-12 17:07:34 +0100

    working flake for ghidra + latest rom downloaded

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Making deadly tower palyable";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default =
+    pkgs.mkShell {
+      packages = with pkgs; [
+        ghidra
+      ];
+    };
+    };
+}
